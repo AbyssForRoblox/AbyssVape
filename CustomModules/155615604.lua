ba53eb4d61f4fc6c17c9c17d523b3e3c90e6fe37
AbyssForRoblox <9dv8jqda@mailmask.me>
2024-11-02 05:48:50 -0700

    auto update

--- CustomModules/155615604.lua
+++ CustomModules/155615604.lua
@@ -101,7 +101,7 @@ local function getcustomassetfunc(path)
 			textlabel:Remove()
 		end)
 		local req = requestfunc({
-			Url = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/"..path:gsub("vape/assets", "assets"),
+			Url = "https://raw.githubusercontent.com/AbyssForRoblox/AbyssVape/main/"..path:gsub("vape/assets", "assets"),
 			Method = "GET"
 		})
 		writefile(path, req.Body)
